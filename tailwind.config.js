1bf812be08e2b53b93c156bc2ddc6a77fed9c68a
MANISH KUMAR SAHU <msahu300@gmail.com>
2023-04-16 20:28:31 +0530

    feat: init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import daisyUI from 'daisyui'
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyUI],
+}
